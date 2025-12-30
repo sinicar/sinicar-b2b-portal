18c868b0ccb03e1f68dfc1efdcb5572f256e2d1c
SiniCar <dev@sinicar.com>
2025-12-31 00:42:39 +0300

    refactor(supplier): add supplier feature barrel exports

---
+++ src/features/supplier/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Supplier Portal Feature Module
+ * وحدة بوابة المورد
+ * 
+ * Clean barrel exports for supplier-related components and views
+ */
+
+// Views
+export * from './views';
+
+// Components
+export * from './components';
